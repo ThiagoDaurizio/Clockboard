a153d7f1f0af7364f210ef790c5f3feffc571ffb
thiagodaurizio <thiago_daurizio@hotmail.com>
2023-12-17 15:46:47 -0300

    style: login page base style

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -10,12 +10,19 @@ const page = () => {
   }
 
   return (
-    <div>
-      <button
-        onClick={login}
-      >
-        LOGIN
-      </button>
+    <div className="w-full min-h-screen flex justify-center items-center">
+      <div className="bg-gray-700 w-[300px] h-fit p-4 rounded-md flex flex-col items-center">
+        <h1 className='text-gray-200 font-semibold tracking-wider text-xl border border-transparent border-b-violet-600 w-[90%] text-center pb-2'>LOGIN</h1>
+        <div className="flex flex-col justify-between flex-1 mt-4">
+          <p className="text-center text-gray-300 text-sm">To use this application you must be logged in and authenticated by a gmail account.</p>
+          <button
+            onClick={login}
+            className="mt-10 bg-violet-600 p-2 px-4 rounded-md text-gray-200 transition-colors duration-300 hover:bg-violet-700"
+          >
+            LOGIN WITH GOOGLE
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
